5a4500b1a0a000a47180eb35e23d584a8e7d1e4b
Rob Toscani <rob_toscani@yahoo.com>
2024-02-11 22:01:36 +0100

    First commit

--- perpetualcalendar3a.sh
+++ perpetualcalendar3a.sh
@@ -29,6 +29,23 @@
 # Overflow bij year > 5 miljoen
 #
 ######################################################################################
+#
+# Copyright (C) 2024 Rob Toscani <rob_toscani@yahoo.com>
+
+# perpetualcalendar3a.sh is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# perpetualcalendar3a.sh is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+######################################################################################
 
 day=$1              # Must be integer > 0
 month=$2            # Must be integer > 0
