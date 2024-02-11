6deac36dcf6b6c09d7e9fb039012135de742a264
Rob Toscani <rob_toscani@yahoo.com>
2024-02-11 22:06:38 +0100

    First commit

--- perpetualcalendar3.py
+++ perpetualcalendar3.py
@@ -31,12 +31,12 @@
 #
 # Copyright (C) 2024 Rob Toscani <rob_toscani@yahoo.com>
 
-# perpetualcalendar3.sh is free software: you can redistribute it and/or modify
+# perpetualcalendar3.py is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# perpetualcalendar3.sh is distributed in the hope that it will be useful,
+# perpetualcalendar3.py is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
